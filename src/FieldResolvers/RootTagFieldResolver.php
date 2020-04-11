90ebdce0b89d87cf08eb0ac24119abf05659841a
Leo <losoviz@gmail.com>
2020-04-11 17:26:48 +0800

    Moved code from trait to class

--- src/FieldResolvers/RootTagFieldResolver.php
+++ src/FieldResolvers/RootTagFieldResolver.php
@@ -2,15 +2,14 @@
 namespace PoP\Taxonomies\FieldResolvers;
 
 use PoP\API\TypeResolvers\RootTypeResolver;
-use PoP\Translation\Facades\TranslationAPIFacade;
-use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
 use PoP\Taxonomies\FieldResolvers\AbstractTagFieldResolver;
-use PoP\Taxonomies\FieldResolvers\RootTagFieldResolverTrait;
+use PoP\Translation\Facades\TranslationAPIFacade;
+use PoP\ComponentModel\Schema\SchemaDefinition;
+use PoP\ComponentModel\TypeResolvers\TypeResolverInterface;
+use PoP\Taxonomies\TypeResolvers\TagTypeResolver;
 
 class RootTagFieldResolver extends AbstractTagFieldResolver
 {
-    use RootTagFieldResolverTrait;
-
     public static function getClassesToAttachTo(): array
     {
         return array(RootTypeResolver::class);
@@ -24,5 +23,74 @@ class RootTagFieldResolver extends AbstractTagFieldResolver
 			'tags' => $translationAPI->__('Tags in the current site', 'pop-taxonomies'),
         ];
         return $descriptions[$fieldName] ?? parent::getSchemaFieldDescription($typeResolver, $fieldName);
+    }
+
+    public static function getFieldNamesToResolve(): array
+    {
+        return array_merge(
+            parent::getFieldNamesToResolve(),
+            [
+                'tag',
+            ]
+        );
+    }
+
+    public function getSchemaFieldType(TypeResolverInterface $typeResolver, string $fieldName): ?string
+    {
+        $types = [
+			'tag' => SchemaDefinition::TYPE_ID,
+        ];
+        return $types[$fieldName] ?? parent::getSchemaFieldType($typeResolver, $fieldName);
+    }
+
+    public function getSchemaFieldArgs(TypeResolverInterface $typeResolver, string $fieldName): array
+    {
+        $schemaFieldArgs = parent::getSchemaFieldArgs($typeResolver, $fieldName);
+        $translationAPI = TranslationAPIFacade::getInstance();
+        switch ($fieldName) {
+            case 'tag':
+                return array_merge(
+                    $schemaFieldArgs,
+                    [
+                        [
+                            SchemaDefinition::ARGNAME_NAME => 'id',
+                            SchemaDefinition::ARGNAME_TYPE => SchemaDefinition::TYPE_ID,
+                            SchemaDefinition::ARGNAME_DESCRIPTION => $translationAPI->__('The tag ID', 'pop-taxonomies'),
+                            SchemaDefinition::ARGNAME_MANDATORY => true,
+                        ],
+                    ]
+                );
+        }
+        return $schemaFieldArgs;
+    }
+
+    public function resolveValue(TypeResolverInterface $typeResolver, $resultItem, string $fieldName, array $fieldArgs = [], ?array $variables = null, ?array $expressions = null, array $options = [])
+    {
+        $cmstaxonomiesapi = \PoP\Taxonomies\FunctionAPIFactory::getInstance();
+        switch ($fieldName) {
+            case 'tag':
+                $query = [
+                    'include' => [$fieldArgs['id']],
+                ];
+                $options = [
+                    'return-type' => POP_RETURNTYPE_IDS,
+                ];
+                if ($tags = $cmstaxonomiesapi->getTags($query, $options)) {
+                    return $tags[0];
+                }
+                return null;
+        }
+
+        return parent::resolveValue($typeResolver, $resultItem, $fieldName, $fieldArgs, $variables, $expressions, $options);
+    }
+
+    public function resolveFieldTypeResolverClass(TypeResolverInterface $typeResolver, string $fieldName, array $fieldArgs = []): ?string
+    {
+        switch ($fieldName) {
+            case 'tag':
+                return TagTypeResolver::class;
+        }
+
+        return parent::resolveFieldTypeResolverClass($typeResolver, $fieldName, $fieldArgs);
     }
 }
