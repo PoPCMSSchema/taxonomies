e067c870afa72b18d9304a0185ec626704cd7c22
Leonardo Losoviz <leo@getpop.org>
2020-09-02 06:14:45 +0800

    Added typehints

--- src/Component.php
+++ src/Component.php
@@ -16,6 +16,11 @@ class Component extends AbstractComponent
     use YAMLServicesTrait;
     // const VERSION = '0.1.0';
 
+    /**
+     * Classes from PoP components that must be initialized before this component
+     *
+     * @return string[]
+     */
     public static function getDependedComponentClasses(): array
     {
         return [
